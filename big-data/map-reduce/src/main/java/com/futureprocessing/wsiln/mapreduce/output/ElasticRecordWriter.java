d5229dab4a028a92333e6a0a64c38fc7d8029012
Szymon Sobocik <ssobocik@future-processing.com>
2015-06-03 09:28:03 +0200

    Semaphores in ElasticRecordWriter

--- big-data/map-reduce/src/main/java/com/futureprocessing/wsiln/mapreduce/output/ElasticRecordWriter.java
+++ big-data/map-reduce/src/main/java/com/futureprocessing/wsiln/mapreduce/output/ElasticRecordWriter.java
@@ -4,31 +4,38 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.elasticsearch.action.ListenableActionFuture;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
 
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class ElasticRecordWriter extends RecordWriter<Text, IntWritable> {
 
+    private static Logger log = LoggerFactory.getLogger(ElasticRecordWriter.class);
+
     private final Client client;
     private final String indexName;
+    private final Semaphore semaphore;
+    private final ElasticResponseHandler elasticResponseHandler;
 
     public ElasticRecordWriter(String indexName, String elasticHost, int elasticPort) {
         this.indexName = indexName;
-        System.out.println("Preparing client to Elastic");
-        System.out.println("Client connection to host and port: " + elasticHost + " : " + elasticPort);
-        System.out.println("Index name: " + indexName);
+        log.info("Preparing client to Elastic");
+        log.info("Client connection to host and port: " + elasticHost + " : " + elasticPort);
+        log.info("Index name: " + indexName);
         this.client = new TransportClient().addTransportAddress(new InetSocketTransportAddress(elasticHost, elasticPort));
-        System.out.println("Client created");
-        System.out.println(client);
+        log.info("Client created: " + client.toString());
+        this.semaphore = new Semaphore(50, true);
+        this.elasticResponseHandler = new ElasticResponseHandler();
     }
 
     @Override
@@ -37,28 +44,35 @@ public class ElasticRecordWriter extends RecordWriter<Text, IntWritable> {
         String t1 = scanner.next();
         String t2 = scanner.next();
 
-        ListenableActionFuture<IndexResponse> execute = client.prepareIndex(indexName, "relations", t1 + "_" + t2)
+        semaphore.acquire();
+        client.prepareIndex(indexName, "relations", t1 + "_" + t2)
                 .setSource(jsonBuilder()
                                 .startObject()
                                 .field("t1", t1)
                                 .field("t2", t2)
                                 .field("v", value.get())
                 )
-                .execute();
+                .execute().addListener(elasticResponseHandler);
 
 
-        ListenableActionFuture<IndexResponse> execute1 = client.prepareIndex(indexName, "list", t1)
+        semaphore.acquire();
+        client.prepareIndex(indexName, "list", t1)
                 .setSource(jsonBuilder()
                                 .startObject()
                                 .field("name", t1)
                 )
-                .execute();
+                .execute().addListener(elasticResponseHandler);
+    }
 
-        try {
-            IndexResponse indexResponse = execute.get();
-            IndexResponse indexResponse1 = execute1.get();
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
+    private class ElasticResponseHandler implements ActionListener<IndexResponse> {
+
+        public void onResponse(IndexResponse indexResponse) {
+            semaphore.release();
+        }
+
+        public void onFailure(Throwable e) {
+            log.error("Error happened while waiting for response from elastic", e);
+            semaphore.release();
         }
     }
 
