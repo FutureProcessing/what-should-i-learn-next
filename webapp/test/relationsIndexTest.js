c9cf4e988349213a9017d92b411bb06701b883a2
exys666 <exys666@gmail.com>
2015-06-09 12:53:40 +0200

    Add test for decreasing value

--- webapp/test/relationsIndexTest.js
+++ webapp/test/relationsIndexTest.js
@@ -29,7 +29,7 @@ var createRelation = function (t1, t2, v) {
     };
 
     return client.index(data(t1, t2, v, true))
-        .then( function () { return client.index(data(t1, t2, v, false)); })
+        .then( function () { return client.index(data(t1, t2, -v, false)); })
         .then( function () { return client.indices.refresh( { index: index, force: true }); });
 };
 
@@ -49,20 +49,41 @@ describe('Relations Index', function () {
 
     it('should find only positive values when no avoid specified', function (done) {
         // given
-        createRelation('java', 'spring', 1000)
+        createRelation('c#', 'linq', 500)
+            .then(createRelation('java', 'spring', 1000))
 
             // when
             .then(function () { return technologies.getSuggestionsRaw(['java'], []) })
 
             // then
             .then(function (results) {
-                console.log(results.aggregations.rel.buckets);
                 var buckets = results.aggregations.rel.buckets;
 
                 expect(buckets).to.have.length(1);
                 expect(buckets[0].key).to.eql('spring');
                 expect(buckets[0].doc_count).to.eql(1);
                 expect(buckets[0].total.value).to.eql(1000);
+
+                done();
+            });
+    });
+
+    it('decrease value if avoid specified', function (done) {
+        // given
+        createRelation('java', 'spring', 1000)
+            .then(createRelation('scala', 'spring', 700))
+
+            // when
+            .then(function () { return technologies.getSuggestionsRaw(['java'], ['scala']) })
+
+            // then
+            .then(function (results) {
+                var buckets = results.aggregations.rel.buckets;
+
+                expect(buckets).to.have.length(1);
+                expect(buckets[0].key).to.eql('spring');
+                expect(buckets[0].doc_count).to.eql(2);
+                expect(buckets[0].total.value).to.eql(300);
 
                 done();
             });
