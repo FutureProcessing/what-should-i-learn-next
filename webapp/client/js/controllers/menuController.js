25dbdfe1d3af04039360f71842ee1c4e0e9b15b7
Michał Adamczyk <madamczyk@future-processing.com>
2015-06-26 13:21:38 +0200

    Menu moved to directive. Added menu for mobile view.

--- webapp/client/js/controllers/menuController.js
+++
@@ -1,7 +0,0 @@
-whatToLearnNextApp.controller('menuController', ['$scope', '$route', '$rootScope', function ($scope, $route, $rootScope) {
-    
-    $scope.$on('$locationChangeSuccess', function(next, current) { 
-        $scope.selected = $route.current.selected;
-    });
-    
-}]);
